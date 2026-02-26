9209cc27723b9338ccc966e9b9e8796b5080ffc8
kitso20 <motlashupingkitso@gmail.com>
2026-02-26 18:57:27 +0200

    Update Main.java

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,7 +1,23 @@
 public class Main {
     public static void main(String[] args){
-        //print out in a new line
+        //print out in a new line and not
+
         System.out.println("i like wine");
         System.out.println("it's really good");
+
+        //variables
+
+        int age = 22;
+        double price = 19.99;
+        char grade = 'A';
+        boolean isWorking = true;
+        // if statement
+        if(isWorking){
+        System.out.println("Good!");
+        }
+        else {
+            System.out.println("Why are you not working?");
+        }
+        String name = "Kitso ";
     }
 }
