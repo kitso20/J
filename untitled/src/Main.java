fa7b484aec0ed02f29b1e61bd13f66c483d029f6
kitso20 <motlashupingkitso@gmail.com>
2026-02-26 15:04:28 +0200

    prints out

---
+++ untitled/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        //print out in a new line
+        System.out.println("i like wine");
+        System.out.println("it's really good");
+    }
+}
