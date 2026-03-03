6c31d5ad063a8dc5538740359ba31111d1bd9215
kitso20 <motlashupingkitso@gmail.com>
2026-03-03 22:14:39 +0200

    noun and adjectives

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -4,11 +4,16 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter a adjective(description): ");
+        String adjective1 = scanner.nextLine();
         System.out.print("Enter a noun (animal or person): ");
+        String noun1 = scanner.nextLine();
         System.out.print("Enter a adjective(description): ");
+        String adjective2 = scanner.nextLine();
         System.out.print("Enter a verb ending with -ing (action): ");
+        String verb = scanner.nextLine();
         System.out.print("Enter a adjective(description): ");
-        
+        String adjective3 = scanner.nextLine();
+
 
         scanner.close();
     }
