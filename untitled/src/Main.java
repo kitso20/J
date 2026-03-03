9416914f2c202d01b6b734b9543823e89975d02d
kitso20 <motlashupingkitso@gmail.com>
2026-03-03 22:30:39 +0200

    Update Main.java

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -6,13 +6,18 @@ public class Main {
         System.out.print("Enter a adjective(description): ");
         String adjective1 = scanner.nextLine();
         System.out.print("Enter a noun (animal or person): ");
-        String noun1 = scanner.nextLine();
+        String noun = scanner.nextLine();
         System.out.print("Enter a adjective(description): ");
         String adjective2 = scanner.nextLine();
         System.out.print("Enter a verb ending with -ing (action): ");
         String verb = scanner.nextLine();
         System.out.print("Enter a adjective(description): ");
         String adjective3 = scanner.nextLine();
+
+        System.out.println("\nToday I went to a " + adjective1 + " zoo.");
+        System.out.println("In an exhibit, I saw a " + noun + ".");
+        System.out.println(noun + " was " + adjective2 + " and " + verb + "!");
+        System.out.println("I was " + adjective3 + "!");
 
 
         scanner.close();
