827001f9cdaf185e8c973ac19b315211195e0c48
kitso20 <motlashupingkitso@gmail.com>
2026-03-08 22:39:22 +0200

    Update Main.java

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -9,6 +9,8 @@ public class Main {
         System.out.println(num1 - num2);
         // '/'
         System.out.println(num1 / num2);
+        //*
+        System.out.println(num1 * num2);
 
     }
 }
