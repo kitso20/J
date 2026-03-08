f93bc46a0921e4ee69e8b6c973a2745cfcf4df59
kitso20 <motlashupingkitso@gmail.com>
2026-03-08 22:37:44 +0200

    add

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,7 +1,10 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
-        int num = 3;
-        System.out.println(num);
+        int num1 = 12;
+        int num2 = 6;
+        //+
+        System.out.println(num1 + num2);
+
     }
 }
