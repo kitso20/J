25c330f827da355245b6ed42ebeb79bafba241fc
kitso20 <motlashupingkitso@gmail.com>
2026-03-10 20:00:17 +0200

    Update Main.java

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -2,8 +2,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         int numbers[] = {1,2,3,4,5,6,7,8};
+        int sun = 0;
         for(int num = 0;num < numbers.length;num++){
-            System.out.print(numbers[num]);
+            sun += numbers[num];
         }
+        System.out.println(sun);
     }
 }
