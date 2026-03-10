d374f74c31cb2b320d2202311c821b578f007c2b
kitso20 <motlashupingkitso@gmail.com>
2026-03-10 19:53:23 +0200

    Update Main.java

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,18 +1,8 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
-        int num1 = 12;
-        int num2 = 6;
-        //+
-        System.out.println(num1 + num2);
-        //-
-        System.out.println(num1 - num2);
-        // '/'
-        System.out.println(num1 / num2);
-        //*
-        System.out.println(num1 * num2);
-        //%
-        System.out.println(num1 % num2);
-
+        for(int num = 1;num < 6;num++){
+            System.out.print(num);
+        }
     }
 }
