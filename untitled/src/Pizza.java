4d3d9e4d18eb5dbd3c5cfc69f985b0cd323c1c92
kitso20 <motlashupingkitso@gmail.com>
2026-03-11 13:23:45 +0200

    main.java to Pizza.java

---
+++ untitled/src/Pizza.java
@@ -0,0 +1,38 @@
+//public class Main {
+//    public static void main(String[] args){
+//        int numbers[] = {1,2,3,4,5,6,7,8};
+//
+//        for (int num : numbers){
+//            System.out.println(num);
+//        }
+//    }
+//}
+public class Pizza {
+
+    private static String cookedCount;
+    private boolean isThinCrust;
+
+    public static class PizzaSalesCounter {
+
+        private static String orderedCount;
+        public static String deliveredCount;
+
+        PizzaSalesCounter() {
+            System.out.println("Static field of enclosing class is "
+                    + Pizza.cookedCount);
+            System.out.println("Non-static field of enclosing class is "
+                    + new Pizza().isThinCrust);
+        }
+    }
+
+    Pizza() {
+        System.out.println("Non private static field of static class is "
+                + PizzaSalesCounter.deliveredCount);
+        System.out.println("Private static field of static class is "
+                + PizzaSalesCounter.orderedCount);
+    }
+
+    public static void main(String[] a) {
+        new Pizza.PizzaSalesCounter();
+    }
+}
