7233f7cdcafaae1bdbb9bd0465ae12a421519e38
kitso20 <motlashupingkitso@gmail.com>
2026-03-11 15:48:06 +0200

    Update Pizza.java

--- untitled/src/Pizza.java
+++ untitled/src/Pizza.java
@@ -7,32 +7,58 @@
 //        }
 //    }
 //}
-public class Pizza {
 
-    private static String cookedCount;
-    private boolean isThinCrust;
 
-    public static class PizzaSalesCounter {
+//public class Pizza {
+//
+//    private static String cookedCount;
+//    private boolean isThinCrust;
+//
+//    public static class PizzaSalesCounter {
+//
+//        private static String orderedCount;
+//        public static String deliveredCount;
+//
+//        PizzaSalesCounter() {
+//            System.out.println("Static field of enclosing class is "
+//                    + Pizza.cookedCount);
+//            System.out.println("Non-static field of enclosing class is "
+//                    + new Pizza().isThinCrust);
+//        }
+//    }
+//
+//    Pizza() {
+//        System.out.println("Non private static field of static class is "
+//                + PizzaSalesCounter.deliveredCount);
+//        System.out.println("Private static field of static class is "
+//                + PizzaSalesCounter.orderedCount);
+//    }
+//
+//    public static void main(String[] a) {
+//        new Pizza.PizzaSalesCounter();
+//    }
+//}
 
-        private static String orderedCount;
-        public static String deliveredCount;
+class Person {
+    final int age;
 
-        PizzaSalesCounter() {
-            System.out.println("Static field of enclosing class is "
-                    + Pizza.cookedCount);
-            System.out.println("Non-static field of enclosing class is "
-                    + new Pizza().isThinCrust);
-        }
+    Person(int age) {
+        this.age = age;
+    }
+}
+
+class Employee extends Person {
+    final String name;
+
+    Employee(String name, int age) {
+        if (age < 18 || age > 67)
+            throw new IllegalArgumentException("Age must be between 18 and 67");
+        super(age); // super() is no longer required as the first statement in Java 25
+        this.name = name;
     }
 
-    Pizza() {
-        System.out.println("Non private static field of static class is "
-                + PizzaSalesCounter.deliveredCount);
-        System.out.println("Private static field of static class is "
-                + PizzaSalesCounter.orderedCount);
-    }
-
-    public static void main(String[] a) {
-        new Pizza.PizzaSalesCounter();
+    public static void main(String[] args) {
+        var emp = new Employee("Alice", 35);
+        System.out.println("Person age set: " + emp.age);
     }
 }
